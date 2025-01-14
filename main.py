6a80e322f0a400eb7d5fbcf33cdbcb561ff88181
Gabriel Picard <gpicard77350@gmail.com>
2025-01-14 23:23:18 +0100

    merge manuel ajout des commandes de restart et de stop dans main)

--- main.py
+++ main.py
@@ -5,9 +5,14 @@
 # Par : Gabychouuu_ #
 # maj : 13/08/2024  #
 #####################
+import asyncio
+from idlelib.colorizer import color_config
+from sys import flags
 
 import discord
+from discord import Interaction, app_commands
 from discord.ext import commands
+
 import play
 import setup
 import registration
@@ -17,12 +22,16 @@ from Database.databasehandler import DatabaseHandler
 from setup import variables_setup as st
 from registration import variables_registration as rg
 import traceback
+import os, sys, subprocess
 
 DatabaseHandler = DatabaseHandler("database.db")
 intents = discord.Intents.all()
 Bot = commands.Bot(command_prefix="!", description="Go pour les LGs", intents=intents)
 discord.AllowedMentions(everyone=True, users=True, roles=True, replied_user=True)
 version = "Alpha 0.9"
+
+ALLOWED_USER_ID = 431481311308546048
+restart_flag = False
 
 @Bot.event
 async def on_ready():
@@ -33,8 +42,10 @@ async def on_ready():
     try:
        synced = await Bot.tree.sync()
        print(f"Synced {len(synced)} commands")
+       msg.add_field(name="commands", value=f"synced {len(synced)} commands", inline=False)
     except Exception as e:
         print(e)
+        msg.add_field(name="error", value=f"Error while command sync")
     await log_channel.send(embed=msg)
 
 @Bot.tree.command()
@@ -106,9 +117,61 @@ async def start(interaction):
 
         print(f"Crash report:\n{error_traceback}")
 
-
 @Bot.tree.command()
 async def reset_game_settings(interaction):
     await reset.reset(interaction, False)
+
+
+@Bot.tree.command(name="restart", description="Redémarre le bot")
+async def restart(interaction: discord.Interaction):
+    try:
+        if interaction.user.id != ALLOWED_USER_ID:
+            await interaction.response.send_message("Vous n'avez pas la permission d'utiliser cette commande.", ephemeral=True)
+            return
+
+        # Répondre à l'interaction
+        await interaction.response.send_message("Redémarrage du bot...\n"
+                                            "Attendre le message de démarrage avant toute chose !", ephemeral=True)
+
+        # Utilisez un autre moyen pour attendre un peu avant de fermer le bot
+        await asyncio.sleep(1)  # Donne un peu de temps pour que le message soit envoyé
+
+        # Quitte le bot
+        subprocess.Popen([sys.executable, *sys.argv])
+        await Bot.close()
+
+    except Exception as e:
+        error_traceback = traceback.format_exc()
+
+        msg = discord.Embed(title="Le Redémarrage a échoué", colour=discord.Colour(0xFF0000))
+        msg.add_field(name="Erreur : ", value=str(e))
+        msg.add_field(name="Détails : ", value=error_traceback, inline=False)
+        await interaction.followup.send(embed=msg)
+
+        print(f"Crash report:\n{error_traceback}")
+
+@Bot.tree.command(name="stop", description="arrête complètement le bot")
+async def stop(interaction):
+    try:
+        if interaction.user.id != ALLOWED_USER_ID:
+            await interaction.response.send_message("Vous n'avez pas la permission d'utiliser cette commande.", ephemeral=True)
+            return
+
+        await interaction.response.send_message("Arrêt du bot...\n")
+
+        await asyncio.sleep(1)
+
+        await Bot.close()
+        quit()
+
+    except Exception as e:
+        error_traceback = traceback.format_exc()
+
+        msg = discord.Embed(title="Le Redémarrage a échoué", colour=discord.Colour(0xFF0000))
+        msg.add_field(name="Erreur : ", value=str(e))
+        msg.add_field(name="Détails : ", value=error_traceback, inline=False)
+        await interaction.followup.send(embed=msg)
+
+        print(f"Crash report:\n{error_traceback}")
 
 Bot.run("MTA0MjkxMjk4Njc3MzMyNzkyMw.GhkAnU.4mQRT_wRgA_Go0ygpETT7zdqHc4haXG1KgCNVY")
