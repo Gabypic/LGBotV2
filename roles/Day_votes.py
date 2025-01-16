4658d4d3566ea3045a488dd49baaae4787c37f12
Gabriel Picard <gpicard77350@gmail.com>
2025-01-16 01:18:27 +0100

    mise a jour majeure du systeme de vote choix (votes jour (fonctionnels mais multiples bugs))

--- roles/Day_votes.py
+++ roles/Day_votes.py
@@ -1,95 +1,95 @@
 import discord
 from discord.ext import commands
+from discord.ui import View, Select
+from select import select
+
 from Database.databasehandler import DatabaseHandler
 from collections import Counter
 import asyncio
-import checks
+
 
 DatabaseHandler = DatabaseHandler("database.db")
 Bot = commands.Bot
 
 
 async def votes(interaction, Bot):
-    users = []
-    player_list = DatabaseHandler.player_list()
-
-    for value in player_list:
-        userid = int(DatabaseHandler.discordID_for_name(value))
-        vivant = DatabaseHandler.is_alive(userid)
-        if vivant:
-            user = await Bot.fetch_user(userid)
-            users.append(user)
-
+    players = []
+    player_list = DatabaseHandler.alive_player_list()
     votes = []
-    voted_users = []
-    votes_complete = False  # Variable de contrôle
+    has_vote = {"count": 0}
+    all_voted = asyncio.Event()
+    votes_complete = False
 
-    print(f"{users} le users \n {len(users)} le len la")
+    for player in player_list:
+        userid = int(DatabaseHandler.discordID_for_name(player))
+        user = await Bot.fetch_user(userid)
+        players.append(user)
 
-    def check(message):
-        print(f"{message.author.id} {message.author.name} {message.content}")
-        return (
-                message.author.id in [user.id for user in users]
-                and message.channel.id == interaction.channel.id
-        )
+    total_player = len(players)
 
-    async def wait_for_votes():
-        nonlocal votes_complete  # Permet de modifier la variable dans la portée extérieure
-        while len(voted_users) < len(users):
-            print("waiting for votes")
-            print(f"{len(voted_users)} voted users, {len(users)} users")
-            voted_message = await Bot.wait_for('message', check=check)
-            print(f"{voted_message.author.id} {voted_message.author.name} {voted_message.content} aaaaaaaaaaah")
-            check_number = checks.check_slash_wait(voted_message)
-            check_max = checks.check_max_player(voted_message.content)
-            print(f"{check_number} number {check_max} max")
 
-            if (voted_message.author.id not in voted_users and DatabaseHandler.is_alive(voted_message.author.id)
-                    and check_number and check_max):
-                voted_users.append(voted_message.author.id)
-                votes.append(voted_message.content)
-                confirmation_msg = discord.Embed(title=f"{voted_message.author} a voté pour "
-                                                       f"{DatabaseHandler.name_for_number(voted_message.content)} !",
-                                                 colour=0x00FF00)
-                await interaction.followup.send(embed=confirmation_msg)
+    async def wait_for_votes(original_interaction):
+        nonlocal votes_complete
 
-            else:
-                if not check_max and DatabaseHandler.is_alive(voted_message.author.id) and check_number:
-                    msg = discord.Embed(title="Le numéro que tu as envoyé n'appartient à aucun joueur", colour=0xFF0000)
-                    await voted_message.author.send(embed=msg)
-                elif not check_number and DatabaseHandler.is_alive(voted_message.author.id):
-                    msg = discord.Embed(title="Il faut voter par le numéro du joueur", colour=0xFF0000)
-                    msg.add_field(name="Il ne doit contenir que le numéro", value="")
-                    await voted_message.author.send(embed=msg)
-                elif not DatabaseHandler.is_alive(voted_message.author.id):
-                    msg = discord.Embed(title="Tu ne peux pas voter en étant mort", colour=0xFF0000)
-                    await voted_message.author.send(embed=msg)
-                else:
-                    msg = discord.Embed(title="Tu ne peux pas voter plusieurs fois", colour=0xFF0000)
-                    await voted_message.author.send(embed=msg)
+        for player in players:
+            msg = discord.Embed(
+                title="Le jour est venu, il est temps de voter pour éliminer un joueur !",
+                colour=discord.Colour.purple()
+            )
+            msg.add_field(
+                name="info",
+                value="Vote pour une cible en la choisissant dans la liste déroulante."
+            )
+            msg.add_field(
+                name="Attention",
+                value="Une fois que tu as voté, tu ne peux pas revenir en arrière !"
+            )
+            options = [discord.SelectOption(label=value) for value in player_list]
+            select = Select(placeholder="Choisis un joueur !", options=options)
 
-        votes_complete = True  # Tous les votes sont terminés
-        print(votes_complete, "salut")
+            async def select_callback(interaction):
+                has_vote["count"] += 1
+                confirmation_msg = discord.Embed(
+                    title=f"Tu as voté pour {select.values[0]}:",
+                    colour=0x00FF00
+                )
+                votes.append(DatabaseHandler.number_for_name(select.values[0]))
+                await interaction.response.send_message(embed=confirmation_msg)
+                general_msg = discord.Embed(
+                    title=f"{player} à voté pour {select.values[0]}",
+                    colour=discord.Colour.purple()
+                )
+                original_interaction.followup.send(embed=general_msg)
+                if has_vote["count"] >= total_player:
+                    all_voted.set()
+
+            select.callback = select_callback
+            view = View()
+            view.add_item(select)
+            await player.send(embed=msg, view=view)
+
+        await all_voted.wait()
+        votes_complete = True
 
     async def time_reminder():
         total_time = 180
         while total_time > 0:
-            if votes_complete:  # Arrête la boucle si les votes sont terminés
+            if votes_complete:
                 break
 
             if total_time == 180 or total_time == 120 or total_time == 60:
-                await interaction.channel.send(f"Il te reste {total_time // 60} minutes pour voter.")
+                await interaction.channel.send(f"Il reste {total_time // 60} minutes pour voter.")
             elif total_time == 30:
-                await interaction.channel.send("Il te reste 30 secondes pour voter !")
+                await interaction.channel.send("Il reste 30 secondes pour voter !")
             elif total_time == 10:
-                await interaction.channel.send("Il te reste 10 secondes pour voter !")
+                await interaction.channel.send("Il reste 10 secondes pour voter !")
 
-            await asyncio.sleep(10)
-            total_time -= 10
+            await asyncio.sleep(2)
+            total_time -= 2
 
     try:
         await asyncio.wait_for(
-            asyncio.gather(wait_for_votes(), time_reminder()),
+            asyncio.gather(wait_for_votes(interaction), time_reminder()),
             timeout=180
         )
     except asyncio.TimeoutError:
